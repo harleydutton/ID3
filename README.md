600bbcc869e52be36b5f4bcb2525b3dbfd2d0c27
Harley Dutton <harleydutton@gmail.com>
2017-12-08 10:16:02 -0700

    cleaned comments. tested entropy(),gain(),and highGainFeat(). all working

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ID3
 ID3 Implementation for Cognitive Science (CS 390L)
+
+
+
+
+
+
+
+IMPORTANT USAGE NOTES BY HARLEY
+if you make a table capable of holding N elements you better fill it up
+i didnt bother to check what happens when it isnt full so it probably breaks
+
+everything is unsafe and there are redundancies everywhere. sorry
--- src/table.java
+++ src/table.java
@@ -14,9 +14,6 @@ public class table {
 
 
     //todo
-    //write something that copies the table omitting various rows and columns
-    //get gain to work
-    //wtf is gain anyway?
     //make the decision tree
 
     public table(int elements, int features){
@@ -32,59 +29,35 @@ public class table {
     }
     public static void main(String[] args){
         base=2.0;
-        table t = new table(4,2);
-        t.add(new String[]{ "green", "square", "B"});
-        t.add(new String[]{"green","square", "B"});
-        t.add(new String[]{"green","circle", "C"});
-        t.add(new String[]{"green","circle", "A"});
-        //t.add(new String[]{"red","square", "A"});
-        //t.add(new String[]{"blue","circle", "C"});
+        table t = new table(10,3);
+        t.add(new String[]{"1","red","square","A"});
+        t.add(new String[]{"1","blue","square","B"});
+        t.add(new String[]{"1","red","circle","C"});
+        t.add(new String[]{"1","blue","circle","D"});
+        t.add(new String[]{"2","red","triangle","A"});
+        t.add(new String[]{"2","blue","triangle","B"});
+        t.add(new String[]{"2","green","triangle","C"});
+        t.add(new String[]{"1","green","square","A"});
+        t.add(new String[]{"2","red","circle","B"});
+        t.add(new String[]{"1","red","triangle","C"});
         System.out.println(t);
-        System.out.println(t.tableEntropy());
+        System.out.println("entropy of t:"+t.tableEntropy());
 
         System.out.println("highest gain feature is:"+t.highestGainFeature());
-        //this should be spitting out "1" as the best feature.
-        //either gain() or highestgainfeature() is broken
-        //figure out how to test these
-        //it was an off ObO error -.-
+        System.out.println("feat 0 gain:"+t.gain(0));
+        System.out.println("feat 1 gain:"+t.gain(1));
+        System.out.println("feat 2 gain:"+t.gain(2));
+        System.out.println("subtables split on feature 1:");
+        for(table temp : t.makeSubTables(1)){
+            System.out.print(temp);
+            System.out.println(rowsep);
+        }
 
-        System.out.println(t.gain(0));
-        System.out.println(t.gain(1));
-
-//        for(table temp : t.makeSubTables(1)){
-//            System.out.println(temp);
-//            System.out.println(rowsep);
-//        }
-//        table[] subtables = t.makeSubTables(1);
-//        table[] indivisible = subtables[1].makeSubTables(0);
-//        for(table asdf : indivisible){
-//            System.out.println(asdf);
-//            System.out.println(rowsep);
-//        }
-//
-//        table[] ihavequestions = subtables[0].makeSubTables(0);
-//        for(table asdf : ihavequestions){
-//            System.out.println(asdf);
-//            System.out.println(rowsep);
-//        }
-        /**
-         *    bucketmanager bm = new bucketmanager();
-         *    for(String[] s : t.guts){
-         *          bm.add(s[s.length-1]);
-         *    }
-         *    System.out.println(bm);
-         *    bucket b = new bucket("blah");
-         *    System.out.println(b);
-         *    System.out.println(bm.numBucks());
-         */
 
     }
     public int highestGainFeature(){
         int bestIndex = -1;
         double bestValue = -1;
-        //for()
-        //iterate through features
-        //do stuff
         for(int i  = 0 ; i < features; i++){
             if(gain(i)>bestValue){
                 bestIndex=i;
@@ -98,19 +71,18 @@ public class table {
         table[] subTables = this.makeSubTables(featNum);
         for(table t : subTables){
             double temp = t.elements*t.tableEntropy()/this.elements;
-            //elements might be the wrong metric to be using.
             out-=temp;
         }
         return out;
     }
 
-    void initAll4Disp(String str){
-        for(int i = 0 ; i < guts.length; i++){
-            for(int j  = 0 ; j < guts[i].length; j++){
-                guts[i][j]=str;
-            }
-        }
-    }
+//    void initAll4Disp(String str){
+//        for(int i = 0 ; i < guts.length; i++){
+//            for(int j  = 0 ; j < guts[i].length; j++){
+//                guts[i][j]=str;
+//            }
+//        }
+//    }
 
     void add(String[] strs){
         if(strs.length!=catIndx){
@@ -128,10 +100,6 @@ public class table {
         guts[addIndex]=strs;addIndex++;
     }
 
-    //rows are features
-    //columns are features and classification
-
-
     public String toString(){
         String out = "";
         if(choiceOrClass!=null)out+=choiceOrClass.toUpperCase()+"\n";
@@ -145,13 +113,6 @@ public class table {
         for(int i = 0 ; i < colMax.length;i++){
             colMax[i]+=2;
         }
-
-//        for(String[] sa : guts){
-//            for(String s : sa){
-//                longest=Math.max(longest,s.length());
-//            }
-//        }
-//        longest+=2;
 
         for(int i= 0 ; i < guts.length; i++){//traverses rows
             for(int j = 0 ; j<guts[0].length;j++) {//traverses columns
@@ -168,15 +129,7 @@ public class table {
         return out;
     }
 
-    //feature# should only ever be input by a loop
-    //im not going to guarantee that the features will be properly numbered
     table[] makeSubTables(int featureNum){
-        //first check to make sure this is in bounds
-        //we shouldnt need to but just to be safe
-        //feature num is going to be zero indexed because it sounds good.
-        //this means this method can take inputs of 0 and it should.
-        //feature num needs to be less than tha total number of features.
-        //this logic handles that problem i think.
         if(featureNum>=features){
             System.out.println("error! not enough features. feat#: " +
                     ""+featureNum+"\n"+this);
@@ -195,39 +148,21 @@ public class table {
         for(int i = 0 ; i < out.length;i++){
             out[i]=new table(bm.bucks.get(i).count,features-1);
             out[i].choiceOrClass=bm.bucks.get(i).name;
-            //out[i].initAll4Disp(""+i);
             blah.put(bm.bucks.get(i).name,out[i]);
 
         }
-        //now i need to figure out how to move the reduced elements into the
-        // new tables... i could make a set of tables indexed by features.
-        //an add method in table would make this work a bit better. then it
-        // needs an index to keep track of how many it has.
-        //my hands are cold. programming outside sucks. to be continued.
-        /*
-        for(iterate through the big table){
-            //i need to make the elements slightly smaller.
-
-            blah.get(bigtableelement.guts[featurenum]).add
-            (bigtableelement[featurenum]);
-        }*/
-        for(String[] sa : guts){//this is the big table. blah is the hashmap
+        for(String[] sa : guts){//this is the big table. blah is the subtables
             String[] reduced = new String[catIndx-1];
             int r = 0;
-            for(int i  = 0; i < sa.length; i++){//iterate through sa
+            for(int i  = 0; i < sa.length; i++){//iterate through sa (bigT)
                 if(i==featureNum) {//logic to skip the String at feat#
                     i++;}
-                //fill em up.
-                reduced[r]=sa[i];
-                r++;
-
+                reduced[r]=sa[i];//fill em up.
+                r++;//keeps track of the skip
             }
             String tabName = sa[featureNum];//i think this doesnt matter now
-            blah.get(tabName).add(reduced);
-
+            blah.get(tabName).add(reduced);//add each element to its subtable
         }
-
-
         return out;
     }
 
@@ -247,17 +182,6 @@ public class table {
         for(int i = 0 ; i < guts.length; i++){
             bm.add(guts[i][guts[i].length-1]);
         }
-
-        //debugging block
-//        System.out.println(bm);
-//        double[] temp = bm.getCategoryHistogram();
-//        String temp2 = "";
-//        for(double d : temp){
-//            temp2+=d+" ";
-//        }
-//        System.out.println(temp2);
-        //debugging block end
-
         return ent(elements,bm.getCategoryHistogram());
     }
     public static double ent(double totalElements, double...
